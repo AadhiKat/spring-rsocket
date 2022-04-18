6c091b77e253e7186defc079435af7e3784c2daf
AadhiKat <aadhithya.hari@meesho.com>
2022-04-18 08:38:58 +0530

    Connection Setup in Spring RSocket

---
+++ src/test/java/com/aadhikat/springrsocket/ConnectionSetupTest.java
@@ -0,0 +1,43 @@
+package com.aadhikat.springrsocket;
+
+import com.aadhikat.springrsocket.dto.ComputationRequestDto;
+import com.aadhikat.springrsocket.dto.ComputationResponseDto;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ConnectionSetupTest {
+
+    @Autowired
+    private RSocketRequester.Builder builder;
+
+    private RSocketRequester requester;
+
+    @BeforeAll
+    public void setup() {
+        this.requester = this
+                .builder
+                .transport(TcpClientTransport.create("localhost" , 6565));
+    }
+
+    @RepeatedTest(3)
+    public void connectionTest() {
+        Mono<ComputationResponseDto> responseDtoMono = this.requester.route("math.service.square")
+                .data(new ComputationRequestDto(ThreadLocalRandom.current().nextInt(1, 50)))
+                .retrieveMono(ComputationResponseDto.class)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(responseDtoMono).expectNextCount(1).verifyComplete();
+    }
+}
